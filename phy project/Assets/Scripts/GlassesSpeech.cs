c53809b91a12f421d73e4fb7dc5a83199ec14482
Emmie Dong <dongemmie99@gmail.com>
2019-10-20 12:23:07 -0400

    Save project to git

---
+++ phy project/Assets/Scripts/GlassesSpeech.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassesSpeech : MonoBehaviour {
+	[SerializeField]
+	Canvas messageCanvas;
+
+	// Use this for initialization
+	void Start () {
+		messageCanvas.enabled = false;
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			TurnOnMessage ();
+		}
+	}
+	private void TurnOnMessage()
+	{
+		messageCanvas.enabled = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			TurnOffMessage ();
+		}
+	}
+	private void TurnOffMessage()
+	{
+		messageCanvas.enabled = false;
+	}
+
+
+}
+
+
+
---
+++ phy project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	float speed = 1.0f;
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.D)) {
+			animator.SetInteger ("AnimState", 1);
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.A)) {
+			animator.SetInteger ("AnimState", 2);
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.S)) { 
+			animator.SetInteger ("AnimState", 3);
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.W)) { 
+			animator.SetInteger ("AnimState", 3);
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		else {
+			animator.SetInteger ("AnimState", 0);
+		}
+
+	}
+}
